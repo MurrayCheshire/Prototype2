8efda42cd9a413279b5e893fda9c95e9a9ae0071
Murray Cheshire <murray.cheshire@outlook.com>
2024-03-08 14:28:45 +0000

    ee

---
+++ Assets/DayTimeController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTimeController : MonoBehaviour
+{
+    const float secondsInDay = 86400.0f;
+    
+    
+    
+    float time;
+}
